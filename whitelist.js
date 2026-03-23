952cd7409e6de15efe01d4f98e3a3caeeab9d5b0
Justinbeber1602 <punrch8@gmail.com>
2026-03-23 21:01:24 +0700

    whitelist.js

--- whitelist.js
+++ whitelist.js
@@ -1,6 +1,4 @@
 {
-    "Peelovehee_tiSSss": true,
-    "Peelovehee_tiSS": "2026-3-23 21:00:00",
-    "Customer_01": "2026-3-24 12:00:00",
-    "New_Buyer_99": "2026-04-01 15:00:00"
+    "Peelovehee_tiSSsss": true,
+    "AnotherUser": "2026-3-23 21:03:00"
 }
